4ce31c508ea7eb5ac0910c305da85e1039fbde61
juancarlospaco <juancarlospaco@gmail.com>
2019-01-04 16:16:31 -0300

    Init

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import faster_than_walk as ftw
 
-print(ftw.walks_simple("sample.csv"))
+print(ftw.walks_simple("/tmp"))
 
-print(ftw.walks_glob("sample.csv"))
+print(ftw.walks_glob("/tmp/*.csv"))
 
-print(ftw.walks("sample.csv"))
+print(ftw.walks("/tmp"))
