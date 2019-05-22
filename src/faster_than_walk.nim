f5c5577aa42627498dc741fcbed0fff13d2175b5
Juan Carlos <juancarlospaco@gmail.com>
2019-05-22 01:10:49 -0300

    improve the core code

--- src/faster_than_walk.nim
+++ src/faster_than_walk.nim
@@ -1,16 +1,7 @@
-## Faster Than Walk
-## ================
-##
-## - Faster recursive directory walk for Python.
-##
-## API
-## ---
-##
-## - ``faster_than_walk.walks()`` Recursive directory walk from path to list of strings.
-## - ``faster_than_walk.walks_glob()`` Recursive directory walk from Glob Pattern to list of strings.
-## - ``faster_than_walk.walks_simple()`` Recursive directory walk from path to list of strings.
-import os, strutils, nimpy
-{.passL: "-s", optimization: speed.}
+import nimpy
+from os import walkDirRec, walkPattern, pcFile, pcDir, pcLinkToDir
+from strutils import normalize, endsWith
+
 
 proc walks*(folderpath: string, extensions: seq[string] = @[""], followlinks : bool = false, yieldfiles: bool = true, debugs: bool = false): seq[string] {.exportpy.} =
   ## Faster os.walk(), followlinks follows SymLinks, yieldfiles yields files else folders, return 1 list of strings.
@@ -24,10 +15,12 @@ proc walks*(folderpath: string, extensions: seq[string] = @[""], followlinks : b
     else:
       result.add item
 
+
 proc walks_glob*(globpattern: string): seq[string] {.exportpy.} =
   ## Faster os.walk() using 1 standard POSIX like Glob Pattern string, return 1 list of strings.
   for item in walkPattern(globpattern):
     result.add item
+
 
 proc walks_simple*(folderpath: string): seq[string] {.exportpy.} =
   ## Faster os.walk(), it only takes the path, return 1 list of strings.
