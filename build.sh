f3adece26a410350c20c62fe4c1720dea33cd9d6
Juan Carlos <juancarlospaco@gmail.com>
2019-12-03 09:03:39 -0300

    Add debug function

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 nim compileToC --compileOnly -d:release --app:lib --opt:speed --gc:markAndSweep --nimcache:. --out:faster_than_walk.so src/faster_than_walk.nim
 
-cp --verbose --force ~/.choosenim/toolchains/nim-0.19.6/lib/nimbase.h nimbase.h
+cp --verbose --force ~/.choosenim/toolchains/nim-1.0.4/lib/nimbase.h nimbase.h
 
 rm --verbose --force faster_than_walk.json
