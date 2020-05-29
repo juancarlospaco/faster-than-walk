7f8ec9f4f0c17e3b527ac6b67df254a81f5c60d8
Juan Carlos <JuanCarlospaco@gmail.com>
2020-05-28 22:44:53 -0300

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nim compileToC --compileOnly -d:release --app:lib --opt:speed --gc:markAndSweep --nimcache:. --out:faster_than_walk.so src/faster_than_walk.nim
+nim compileToC --compileOnly -d:danger --app:lib --opt:speed --gc:markAndSweep --nimcache:. --out:faster_than_walk.so src/faster_than_walk.nim
 
 cp --verbose --force ~/.choosenim/toolchains/nim-1.0.4/lib/nimbase.h nimbase.h
 
