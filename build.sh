38fd83b839f712e508f161b6dc3ca28dbbb7a21e
Juan Carlos <JuanCarlospaco@gmail.com>
2020-06-05 20:01:17 -0300

    re fix the fix

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-nim compileToC --compileOnly -d:danger --app:lib --opt:speed --gc:markAndSweep --nimcache:. --out:faster_than_walk.so src/faster_than_walk.nim
-
-cp --verbose --force ~/.choosenim/toolchains/nim-1.0.4/lib/nimbase.h nimbase.h
-
-rm --verbose --force faster_than_walk.json
