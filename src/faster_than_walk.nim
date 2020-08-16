07fba9a4c749a16aa3e6694df1c6481ff86e41d9
Juan Carlos <JuanCarlospaco@gmail.com>
2020-08-16 16:06:33 -0300

    Update new version

--- src/faster_than_walk.nim
+++ src/faster_than_walk.nim
@@ -1,17 +1,17 @@
 import os, strutils, nimpy
 
 
-proc walk*(folderpath: string, extensions: seq[string] = @[""], followlinks : bool = false, yieldfiles: bool = true, debugs: bool = false): seq[string] {.exportpy.} =
+proc walk*(folderpath: string, extensions: seq[string] = @[""], followlinks : bool = false, yieldfiles: bool = true, debugs: bool = false, check_folders: bool = false): seq[string] {.exportpy.} =
   ## Faster os.walk(), followlinks follows SymLinks, yieldfiles yields files else folders, return 1 list of strings.
   let extused = create(bool, sizeOf(bool))  # Optimization.
   extused[] = extensions != @[""] and extensions.len > 0
-  for item in walkDirRec(folderpath, {if yieldfiles: pcFile else: pcDir}, {if followlinks: pcLinkToDir else: pcDir}):
+  for item in walkDirRec(folderpath, {if yieldfiles: pcFile else: pcDir}, {if followlinks: pcLinkToDir else: pcDir}, checkDir=check_folders):
     if unlikely(debugs): echo item
     if unlikely(extused[]):
       for ext in extensions:
         if item.normalize.endsWith(ext): result.add item
     else: result.add item
-  zeroMem(extused, sizeOf(extused.type))    # Optimization.s
+  dealloc extused
 
 
 proc walk_glob*(globpattern: string): seq[string] {.exportpy.} =
@@ -19,12 +19,12 @@ proc walk_glob*(globpattern: string): seq[string] {.exportpy.} =
   for item in walkPattern(globpattern): result.add item
 
 
-proc walk_simple*(folderpath: string, recursive: bool = true, relative: bool = false): seq[string] {.exportpy.} =
+proc walk_simple*(folderpath: string, recursive: bool = true, relative: bool = false, check_folders: bool = false): seq[string] {.exportpy.} =
   ## Faster os.walk(), it only takes the path, return 1 list of strings.
   if likely(recursive):
-    for item in walkDirRec(folderpath): result.add item
+    for item in walkDirRec(folderpath, relative=relative, checkDir=check_folders): result.add item
   else:
-    for item in walkDir(folderpath, relative): result.add item.path
+    for item in walkDir(folderpath, relative, checkDir=check_folders): result.add item.path
 
 
 proc walk_files*(globpattern: string, folders_only: bool = false): seq[string] {.exportpy.} =
@@ -33,11 +33,3 @@ proc walk_files*(globpattern: string, folders_only: bool = false): seq[string] {
     for item in walkDirs(globpattern): result.add item
   else:
     for item in walkFiles(globpattern): result.add item
-
-
-proc debugs*() {.discardable, exportpy.} =
-  ## Get the Config and print it to the terminal, for debug purposes only, human friendly.
-  echo static({
-    "nimVersion": NimVersion, "cpu": hostCPU, "os": hostOS, "endian": $cpuEndian, "release": $defined(release), "danger": $defined(danger),
-    "CompileDate": CompileDate,  "CompileTime": CompileTime, "tempDir": getTempDir(), "currentCompilerExe": getCurrentCompilerExe(), "int.high": $int.high
-  })
