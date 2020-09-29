343228be7609118308af67289d4ac4aeef32aaa3
Juan Carlos <JuanCarlospaco@gmail.com>
2020-09-29 09:23:13 -0300

    Add noalias optimizations

--- src/faster_than_walk.nim
+++ src/faster_than_walk.nim
@@ -3,7 +3,7 @@ import os, strutils, nimpy
 
 proc walk*(folderpath: string, extensions: seq[string] = @[""], followlinks : bool = false, yieldfiles: bool = true, debugs: bool = false, check_folders: bool = false): seq[string] {.exportpy.} =
   ## Faster os.walk(), followlinks follows SymLinks, yieldfiles yields files else folders, return 1 list of strings.
-  let extused = create(bool, sizeOf(bool))  # Optimization.
+  let extused {.noalias.} = create(bool, sizeOf(bool))  # Optimization.
   extused[] = extensions != @[""] and extensions.len > 0
   for item in walkDirRec(folderpath, {if yieldfiles: pcFile else: pcDir}, {if followlinks: pcLinkToDir else: pcDir}, checkDir=check_folders):
     if unlikely(debugs): echo item
@@ -19,17 +19,16 @@ proc walk_glob*(globpattern: string): seq[string] {.exportpy.} =
   for item in walkPattern(globpattern): result.add item
 
 
-proc walk_simple*(folderpath: string, recursive: bool = true, relative: bool = false, check_folders: bool = false): seq[string] {.exportpy.} =
+proc walk_simple*(folderpath: string, relative: bool = false, check_folders: bool = false): seq[string] {.exportpy.} =
   ## Faster os.walk(), it only takes the path, return 1 list of strings.
-  if likely(recursive):
-    for item in walkDirRec(folderpath, relative=relative, checkDir=check_folders): result.add item
-  else:
-    for item in walkDir(folderpath, relative, checkDir=check_folders): result.add item.path
+  for item in walkDirRec(folderpath, relative=relative, checkDir=check_folders): result.add item
 
 
-proc walk_files*(globpattern: string, folders_only: bool = false): seq[string] {.exportpy.} =
+proc walk_folders*(globpattern: string): seq[string] {.exportpy.} =
   ## Faster os.walk(), similar to walks but optimized for Files or Folders only.
-  if unlikely(folders_only):
-    for item in walkDirs(globpattern): result.add item
-  else:
-    for item in walkFiles(globpattern): result.add item
+  for item in walkDirs(globpattern): result.add item
+
+
+proc walk_files*(globpattern: string): seq[string] {.exportpy.} =
+  ## Faster os.walk(), similar to walks but optimized for Files or Folders only.
+  for item in walkFiles(globpattern): result.add item
