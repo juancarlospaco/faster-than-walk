95ebb0b804992673c27cfd215127b562456a22d5
Juan Carlos <JuanCarlospaco@gmail.com>
2021-02-24 17:40:54 -0300

    .

--- package4pypi.sh
+++ package4pypi.sh
@@ -1,4 +1,4 @@
-rm --verbose --force --recursive dist/faster_than_requests/faster_than_requests/
-rm --verbose --force --recursive dist/faster_than_requests/faster_than_requests/__pycache__/
-cp --verbose --recursive faster_than_requests/ dist/faster_than_requests/
-cd dist && zip -9 -T -v -r faster_than_requests.zip *
+rm --verbose --force --recursive dist/faster_than_walk/faster_than_walk/
+rm --verbose --force --recursive dist/faster_than_walk/faster_than_walk/__pycache__/
+cp --verbose --recursive faster_than_walk dist/faster_than_walk/
+cd dist && zip -9 -T -v -r faster_than_walk.zip *
